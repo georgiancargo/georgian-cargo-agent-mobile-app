361dca7c73fc3e85043ce8bfbad9d7e712fd7248
Sara Samer <ss.titanic199@gmail.com>
2020-12-14 07:30:08 +0200

    Added context aliases

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
           _styles: './src/styles',
           _utils: './src/utils',
           _hooks: './src/hooks',
+          _context: './src/context', 
         },
       },
     },
