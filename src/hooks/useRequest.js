d52ae6aaf43876d1bc759059b2aca64b59f6cbff
Sara-Samer <ss.titanic199@gmail.com>
2020-12-24 17:23:03 +0200

    update

--- src/hooks/useRequest.js
+++ src/hooks/useRequest.js
@@ -1,11 +1,29 @@
 import {useContext, useState} from "react";
 import useAxios from "./useAxios";
 import {AuthContext} from "_context";
+import {NetInfo, Platform} from "react-native";
 
 export default function useRequest(request, load = false) {
     const axios = useAxios();
     const [isProcessing, setIsProcessing] = useState(false);
     const {setAuth} = useContext(AuthContext);
+
+    NetInfo.isConnected.addEventListener(
+        "connectionChange",
+        handleFirstConnectivityChange
+    );
+    const handleFirstConnectivityChange = (isConnected) => {
+        // NetInfo.isConnected.removeEventListener(
+        //     "connectionChange",
+        //     this.handleFirstConnectivityChange
+        // );
+        if (isConnected === false) {
+            Alert.alert("You are offline!");
+        } else {
+            Alert.alert("You are online!");
+        }
+    };
+
     const send = async (data) => {
         setIsProcessing(true);
         const response = request(axios, data)
