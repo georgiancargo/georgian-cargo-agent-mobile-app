089814ba011cdfb9ebb9ab5a2c3dd5757c234327
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2021-01-11 01:58:15 +0200

    Offline requests

--- src/hooks/useOfflineRequest.js
+++ src/hooks/useOfflineRequest.js
@@ -1,9 +1,10 @@
 import {useContext, useState} from "react";
 import useAxios from "./useAxios";
 import {AuthContext} from "_context";
-// import {setItemAsync as set, getItemAsync as get} from "expo-secure-store";
+//import {setItemAsync as set, getItemAsync as get} from "expo-secure-store";
 import {parseRequest} from "_requests";
-import {setItem as set, getItem as get} from '@react-native-async-storage/async-storage';
+// import { getItemAsync as get, setItemAsync as set } from 'react-native';
+import * as fs from "expo-file-system"
 
 export default function useOfflineRequest(r, load = false) {
     const axios = useAxios();
@@ -45,18 +46,35 @@ export default function useOfflineRequest(r, load = false) {
                 });
         else {
             let requests = [];
-            const store = "requests";
+            const path = fs.cacheDirectory + '/offline_requests.json';
+            try {
+                const raw = await fs.readAsStringAsync(path);
+                requests = JSON.parse(raw);
+            } catch (e) {
+                // Nothing to do
+            }
+            requests.push(config);
+            fs.writeAsStringAsync(path, JSON.stringify(requests))
+                .catch((e) => {
+                })
+                .finally(() => {
+                    setIsProcessing(false)
+                })
+            /* const store = "requests";
             get(store)
                 .then((res) => {
+                    alert("Done");
                     if (res !== null) requests = JSON.parse(res);
                     requests.push(config);
                     return set(store, JSON.stringify(requests));
                 })
-                .then(() => {})
-                .catch((e) => {})
+                .catch((e) => {
+                    alert("Error" + e);
+                })
                 .finally(() => {
                     setIsProcessing(false);
                 });
+             */
         }
     };
     return [send, isProcessing];
