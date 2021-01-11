7a9a5491fc88e8a1f54307a896cc5ca0eacfd487
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2021-01-11 20:16:01 +0200

    Visual cue s

--- src/components/molecules/ParcelList.js
+++ src/components/molecules/ParcelList.js
@@ -4,7 +4,7 @@ import {ListItem, ModalContainer, Button} from "_atoms";
 import BootstrapStyleSheet from "react-native-bootstrap-styles";
 import {Divider, useTheme} from "react-native-paper";
 import {AuthContext} from "_context";
-import { ScrollView } from "react-native";
+import {ScrollView} from "react-native";
 
 const bootstrapStyleSheet = new BootstrapStyleSheet();
 const {s, c} = bootstrapStyleSheet;
@@ -17,10 +17,10 @@ const ParcelList = ({parcels = [], navigation}) => {
     const canEdit = auth.agent.privileges.includes("AMEND_CARGO_INFORMATION");
 
     const labels = ["Tracking number", "Weight", "Status", "From", "To", "Collection option", "Customer type", "Parcel type", "Notes", "Description", "Customer id", "Created at", "Release code", "Currency code", "Freight price", "Delivery price", "Discount"];
-    const keys = ["tracking_number", "weight", "status", "source_country_code", "destination_country_code", "collection_option", "customer_type", "parcel_type", "notes", "description", "customer_id", "created_at", "release_code",  "currency_code", "freight_price", "delivery_price", "discount"];
+    const keys = ["tracking_number", "weight", "status", "source_country_code", "destination_country_code", "collection_option", "customer_type", "parcel_type", "notes", "description", "customer_id", "created_at", "release_code", "currency_code", "freight_price", "delivery_price", "discount"];
     const userLabels = ["Name", "Email", "Phone", "Address line 1", "Address line 2", "Postal code"];
     const userKeys = ["name", "email", "phone", "address_line_1", "address_line_2", "postal_code"];
-   
+
     const showModal = (parcel) => {
         const {shipping_specs, item, invoice, ...p} = parcel;
         const {
@@ -50,9 +50,9 @@ const ParcelList = ({parcels = [], navigation}) => {
         hideModal();
         navigation.navigate("Edit Parcel", {parcel: parcel});
     };
-    const renderItem = ({item, index}) => <ListItem parcel={item} edit={showModal} i={index} />;
+    const renderItem = ({item, index}) => <ListItem parcel={item} edit={showModal} i={index}/>;
 
-    const ParcelInfoModal = () =>{ 
+    const ParcelInfoModal = () => {
         const receiver = parcel.receiver;
         const sender = parcel.sender;
         const style = {
@@ -68,26 +68,35 @@ const ParcelList = ({parcels = [], navigation}) => {
         };
         const Parcel = () =>
             keys.map((key, i) => (
-                <View style={style.row} key={key}>
-                    <Text style={style.dd}>{labels[i]}</Text>
-                    <Text style={style.dt}>{parcel[key] ? parcel[key] : "N/A"}</Text>
-                </View>
+                <>
+                    <View style={style.row} key={key}>
+                        <Text style={style.dd}>{labels[i]}</Text>
+                        <Text style={style.dt}>{parcel[key] ? parcel[key] : "N/A"}</Text>
+                    </View>
+                    <Divider style={{marginVertical: 3}}/>
+                </>
             ));
         const Sender = () =>
             sender &&
             userKeys.map((key, i) => (
-                <View style={style.row} key={key}>
-                    <Text style={style.dd}>{"Sender " + userLabels[i]}</Text>
-                    <Text style={style.dt}>{sender[key] ? sender[key] : "N/A"}</Text>
-                </View>
+                <>
+                    <View style={style.row} key={key}>
+                        <Text style={style.dd}>{"Sender " + userLabels[i]}</Text>
+                        <Text style={style.dt}>{sender[key] ? sender[key] : "N/A"}</Text>
+                    </View>
+                    <Divider style={{marginVertical: 3}}/>
+                </>
             ));
         const Receiver = () =>
             receiver &&
             userKeys.map((key, i) => (
-                <View style={style.row} key={key}>
-                    <Text style={style.dd}>{"Receiver " + userLabels[i]}</Text>
-                    <Text style={style.dt}>{receiver[key] ? receiver[key] : "N/A"}</Text>
-                </View>
+                <>
+                    <View style={style.row} key={key}>
+                        <Text style={style.dd}>{"Receiver " + userLabels[i]}</Text>
+                        <Text style={style.dt}>{receiver[key] ? receiver[key] : "N/A"}</Text>
+                    </View>
+                    <Divider style={{marginVertical: 3}}/>
+                </>
             ));
         return (
             <ModalContainer
@@ -95,11 +104,11 @@ const ParcelList = ({parcels = [], navigation}) => {
                 setModalVisible={setModalVisible}
             >
                 <ScrollView style={[s.container]}>
-                    <Parcel />
-                    <Divider style={{marginVertical:3}} />
-                    <Sender />
-                    <Divider style={{marginVertical:3}} />
-                    <Receiver />
+                    <Parcel/>
+                    <Divider style={{marginVertical: 3}}/>
+                    <Sender/>
+                    <Divider style={{marginVertical: 3}}/>
+                    <Receiver/>
                     <View style={style.buttonRow}>
                         <Button
                             style={{flex: 8, marginHorizontal: 2}}
@@ -117,11 +126,12 @@ const ParcelList = ({parcels = [], navigation}) => {
                     </View>
                 </ScrollView>
             </ModalContainer>
-        );}
+        );
+    }
     return (
         <>
             <SafeAreaView style={styles.container}>
-                <ParcelInfoModal />
+                <ParcelInfoModal/>
                 <FlatList
                     data={parcels}
                     renderItem={renderItem}
