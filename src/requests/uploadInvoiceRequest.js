45b0cbb6f3ecbdcdbecaa5ad72b3edfb911f0d98
Sara-Samer <ss.titanic199@gmail.com>
2021-01-17 18:52:53 +0200

    uploading file name

--- src/requests/uploadInvoiceRequest.js
+++ src/requests/uploadInvoiceRequest.js
@@ -1,11 +1,16 @@
 export default function uploadInvoiceRequest(axios, data) {
-    const {invoice_id, invoice} = data;
-    var photo = {uri: invoice, name:  'test.png', type:'image/png'};
-    var body = new FormData();
+    const {invoice_id, invoice = ""} = data;
+    const filename = invoice.split("/").pop();
+    const filetype = filename.split(".").pop();
+    // const photo = {uri: invoice, name:  filename, type:filetype};
+    
+    const photo = {uri: invoice, name: filename, type: "image/png"};
+    const body = new FormData();
+    
     body.append("invoice", photo);
-    return axios.post(`/cargo/invoice/upload/${invoice_id}`, body,{
+    return axios.post(`/cargo/invoice/upload/${invoice_id}`, body, {
         headers: {
-            'content-type': 'multipart/form-data'
-        }
+            "content-type": "multipart/form-data",
+        },
     });
 }
