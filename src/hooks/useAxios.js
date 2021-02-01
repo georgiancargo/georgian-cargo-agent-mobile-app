6a8620c167b270fce4c74cf6b5d7bd793fc73198
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2021-02-01 11:02:35 +0200

    Fix https route

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -6,7 +6,7 @@ export default function useAxios() {
     const {auth} = useContext(AuthContext);
     const {access_token} = {...auth};
 
-    const baseURL = "http://api.georgiancargo.co.uk";
+    const baseURL = "https://api.georgiancargo.co.uk";
 
     const defaultAxios = Axios.create({
         baseURL,
